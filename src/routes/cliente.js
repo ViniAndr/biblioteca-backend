cf19c2b2f7912e4e9ffb3a016edbb82cdb8c4473
Vinicius Andrade <fabioviniciusbarbosa@hotmail.com>
2025-01-03 22:57:29 -0300

    controller, routes e service do cliente, e instalação do prisma CLI

---
+++ src/routes/cliente.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+const router = Router();
+
+/* verifica se o CLIENTE já tem o cadastro simples e atualiza adicionando o email e senha,
+   caso não tenha, ele fará um cadastro completo. */
+
+// cadastro completo, ou seja, o online feito pelo cliente.
+
+// cadastro simples, ou seja, o presencial feito pelo funcionario
+
+// login do cliente
+
+// ver seus dados (PERFIL)
+
+// atualizar seus dados de regsitro (email, senha, telefone e nome)
+
+// atualizar seus dados de endereço
+
+// atualizar todos os dados do cliente, porem quem faz isso é o funcionario.
+
+// listar todos os clientes (Funcionario).
+
+// ver informações de um cliente por ID (Funcionario).
+
+export default router;
